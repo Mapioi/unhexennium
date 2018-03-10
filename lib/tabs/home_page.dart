1592ff737704db48e829f9c2e112c2b4fbef86ac
TrakJohnson <trakjohnsonweb@gmail.com>
2018-03-10 21:11:13 +0100

    Add simple tabs skeleton

--- lib/tabs/home_page.dart
+++ lib/tabs/home_page.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
 
-enum Mode { Default, Element, Formula, Equation }
+// constants
+enum Mode { Element, Formula, Equation }
 
+const default_mode = Mode.Element;
+
+// utils
+String enumToString(enumElement) => enumElement.toString().split(".")[1];
+
+// ui
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  Mode _mode = Mode.Default;
+  Mode _mode = default_mode;
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return new DefaultTabController(
+      length: Mode.values.length,
+      child: new Scaffold(
         appBar: new AppBar(
           title: new Text("Unhexennium"),
+          bottom: new TabBar(
+            isScrollable: true,
+            tabs: Mode.values.map((Mode tabTitle) {
+              return new Tab(
+                  text: enumToString(tabTitle)
+              );
+            }).toList(),
+          ),
         ),
-        body: new Center(
-          child: new Text(_mode.toString()),
-        ));
+        // content
+        body: new TabBarView(
+          children: Mode.values.map((Mode tabTitle) {
+            return new Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: new Text(enumToString(tabTitle)),
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
